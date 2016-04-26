138763bbd885311888b58649a36bff7596f37c66
Brandon Joyce <bjoyce@covermymeds.com>
2016-04-26 08:18:45 -0400

    Fixes for shaw registration pages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,16 +1,20 @@
 class RegistrationsController < ApplicationController
   def new
-    # renders the registration page
-    @voter = Voter.new
-    @schools = School.all.map { |s| [s.name, s.id] }
+    if session[:voter]
+      redirect_to action: 'show', id: 1
+    else
+      # renders the registration page
+      @voter = Voter.new
+      @schools = School.all.map { |s| [s.name, s.id] }
+    end
   end
-  
+
   def create
     # 1. Try submitting the test form at /registrations/new and hitting this breakpoint. (explore the 'params' variable)
     # binding.pry
-    
+
     voter = nil
-    
+
     # 2. Implement the 'unauthenticated voter' scenario - where the voter needs to be created without a registration number
     if registration_number.blank?
       voter = Voter.create!(voter_params.merge(registration_number: RegistrationNumber.next))
@@ -19,24 +23,24 @@ class RegistrationsController < ApplicationController
       voter = Voter.find_by(registration_number: registration_number, school_id: voter_params[:school_id])
       voter.update_attributes!(voter_params)
     end
-  
-    # 4. Store the registered user in 'session' (we can discuss what session is)  
+
+    # 4. Store the registered user in 'session' (we can discuss what session is)
     session[:voter] = voter
     redirect_to action: 'show', id: 1
   end
-  
+
   # renders the success page
   def show
     # TODO - Get the registered user from session if present
     @voter = session[:voter]
   end
-  
-  private 
-  
+
+  private
+
   def registration_number
     params[:voter][:registration_number]
   end
-  
+
   def voter_params
     params.require(:voter).permit(:school_id, :gender, :school_year, :will_be_eighteen)
   end
