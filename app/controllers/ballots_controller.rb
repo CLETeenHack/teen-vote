6787960f872622360ae43b26656579c292878877
Lindsey <17lclark@beaumontschool.org>
2016-04-28 02:19:23 +0000

    move sample ballot to ballots controller

---
+++ app/controllers/ballots_controller.rb
@@ -0,0 +1,15 @@
+class BallotsController < ApplicationController
+  def index
+    @issues = Issue.all
+    @national_issues = Issue.joins(:issue_type)
+                            .where(issue_types: {national: true})
+                            .all
+    @state_issues = Issue.joins(:issue_type)
+                         .where(issue_types: {state: true})
+                         .all
+  end
+
+  def show
+    @issue = Issue.find(params[:id])
+  end
+end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -14,18 +14,32 @@ class VotesController < ApplicationController
   def create
     voter = Voter.find(params[:voter_id])
     voter.votes.destroy_all
-    if params[:issue]
+    if issue_params
       issues = Issue.where(id:params[:issue].keys)
       choices = issues.map{|i|i.choices.where(id:params[:issue][i.id.to_s])}.flatten
       choices.each do |choice|
-        Vote.create!(voter_id: voter.id, choice_id: choice.id)
+        issue_id = choice.issue_id.to_s
+        Vote.create!(
+          voter_id: voter.id, 
+          choice_id: choice.id,
+          understand_this: understand_this_params[issue_id],
+          care_about_this: care_about_this_params[issue_id]
+        )
       end
     end
   end
 
   private
 
-  def vote_params
-    params.require(:vote).permit(:name, :description, :bulk_quantity, :price, :last_ordered, :stock_item)
+  def issue_params
+    params.require(:issue)
+  end
+
+  def understand_this_params
+    params.require(:understand_this)
+  end
+
+  def care_about_this_params
+    params.require(:care_about_this)
   end
 end
