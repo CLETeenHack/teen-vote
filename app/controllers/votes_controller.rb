ba8df29ed7d8a5daa9caab4778721735a4dc05c0
Joel Byler <jbyler@covermymeds.com>
2016-05-04 20:11:33 -0400

    Final cleanup for Be@uTech

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,19 @@
 class VotesController < ApplicationController
 
   def new
-    #TODO: after registration system is in place, use the active voter instead of just the first one
-    @voter = Voter.first
-    @issues = Issue.all
-    @national_issues = Issue.joins(:issue_type)
-                            .where(issue_types: {national: true})
-                            .all
-    @state_issues = Issue.joins(:issue_type)
-                         .where(issue_types: {state: true})
-                         .all
+
+    unless session[:voter]
+      redirect_to new_registration_path
+    else
+      @voter = Voter.find_by(session[:voter]['id'])
+      @issues = Issue.all
+      @national_issues = Issue.joins(:issue_type)
+                              .where(issue_types: {national: true})
+                              .all
+      @state_issues = Issue.joins(:issue_type)
+                           .where(issue_types: {state: true})
+                           .all
+    end
   end
   def create
     voter = Voter.find(params[:voter_id])
@@ -20,7 +24,7 @@ class VotesController < ApplicationController
       choices.each do |choice|
         issue_id = choice.issue_id.to_s
         Vote.create!(
-          voter_id: voter.id, 
+          voter_id: voter.id,
           choice_id: choice.id,
           understand_this: understand_this_params[issue_id],
           care_about_this: care_about_this_params[issue_id]
@@ -30,7 +34,7 @@ class VotesController < ApplicationController
   end
   def thanks_for_voting
   end
-  
+
   def results
     @issues = Issue.all
     @national_issues = Issue.joins(:issue_type)
@@ -53,8 +57,12 @@ class VotesController < ApplicationController
   def care_about_this_params
     params.require(:care_about_this)
   end
+
   def index
     @issues = Issue.all
   end
-  
+
+  def voter
+    Voter.find_by(session[:voter]['id'])
+  end
 end
